c8c1e5da6c2748e788cc8a4eb748ca3dec7ae7d5
didil <faridfadilah70@gmail.com>
2026-06-16 22:26:45 +0700

    footer sementara

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    @vite(['resources/css/footer.css'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<style>
+    * {
+        top: 0;
+        left: 0;
+        margin: 0;
+        padding: 0;
+        font-family: 'Poppins', serif;
+    }
+</style>
+
+<body>
+    <header class="header">
+        <navbar>
+
+        </navbar>
+    </header>
+    <div class="main-content">
+
+    </div>
+    <footer class="footer">
+        <div class="top">
+            <div class="left">
+                <h2>DielasCode.</h3>
+                <p>Building Digital Solutions with Code and Creativity.</p>
+            </div>
+            <div class="right">
+                <ul>
+                    <li><i class="fa-solid fa-at"></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><i class="fa-brands fa-youtube"></i></li>
+                    <li><i class="fa-brands fa-linkedin"></i></li>
+                    <li><i class="fa-brands fa-github"></i></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="bottom">
+            <p>© 2026 DielasCode. Hak Cipta Dilindungi.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
